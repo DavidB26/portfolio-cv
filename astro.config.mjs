8aa8ad1be933edcaef73f138c69847d4ef828e6f
David <luisdavidbeslanga@gmail.com>
2025-11-17 09:42:20 -0500

    ajustes de skills y proyectos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
-// https://astro.build/config
+import mdi from '@iconify-json/mdi/icons.json';
+import ri from '@iconify-json/ri/icons.json';
+import fa6solid from '@iconify-json/fa6-solid/icons.json';
+import logos from '@iconify-json/logos/icons.json';
+
 export default defineConfig({
+  integrations: [
+    icon({
+      collections: {
+        logos,
+        mdi,
+        ri,
+        'fa6-solid': fa6solid,
+      },
+    }),
+  ],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
